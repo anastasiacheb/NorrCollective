42e2cf1fdd6e9b795eda1497c9bb170e4fdce590
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-07-11 21:24:21 +0300

    shop link section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
               </div>
               <div className="flex gap-0.5 items-center">
                 <img src="/icons/call.svg" alt="icon" className="size-6" />
-                <p className="font-semibold">+46 8 123 45 678</p>
+                <p className="font-semibold">+46 8 876 54 321</p>
               </div>
             </div>
           </div>
@@ -238,6 +238,18 @@ export default function Home() {
             </div>
           </div>
         </div>
+      </section>
+      <section className="px-4 py-10 md:p-20 border-x border-b border-base-900">
+        <h2 className="text-[34px] font-semibold md:text-[50px] text-center mb-4">Discover Our Iconic Pieces</h2>
+        <p className="text-center leading-snug mb-14 text-lg">
+          Explore our collection of carefully selected furniture and add timeless character{' '}
+          <br className="hidden md:block" /> to your interior. Click the button below to start shopping.
+        </p>
+        <Link
+          href="/shop"
+          className="bg-base-900 text-base-0 hover:bg-base-700 transition-all ease-linear uppercase text-sm leading-none font-medium border border-base-900 md:text-base h-12 md:h-14 flex items-center justify-center max-w-67 mx-auto">
+          shop now
+        </Link>
       </section>
     </>
   );
