edbd1fb826f43548f0b60ef776ce52035f7cf82b
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-08-04 13:08:10 +0300

    links

---
+++ src/app/shop/[category]/[product]/page.tsx
@@ -0,0 +1,70 @@
+import { notFound } from 'next/navigation';
+import { ProductLink, ProductPage } from '@/components';
+
+function CreateSlug(string: string) {
+  return string.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+}
+
+interface Product {
+  name: string;
+  price: string;
+  description: string;
+  src: string[];
+}
+
+interface Category {
+  category: string;
+  href: string;
+  src1: string;
+  products: Product[];
+}
+
+export async function generateStaticParams() {
+  const cats: Category[] = await fetch('https://norrcollective.vercel.app/api/products').then((res) => res.json());
+
+  return cats.flatMap((cat) =>
+    cat.products.map((product) => ({
+      category: cat.href.replace('/', ''),
+      product: CreateSlug(product.name),
+    })),
+  );
+}
+
+export default async function Page({ params }: { params: Promise<{ category: string; product: string }> }) {
+  const { category, product } = await params;
+  const cats: Category[] = await fetch('https://norrcollective.vercel.app/api/products').then((res) => res.json());
+  const cat = cats.find((item) => item.href.replace('/', '') === category.toLowerCase());
+
+  if (!cat) {
+    notFound();
+  }
+
+  const prod = cat.products.find((item) => CreateSlug(item.name) === product);
+
+  if (!prod) {
+    notFound();
+  }
+
+  const otherProducts = cats.flatMap((item) => item.products).filter((item) => CreateSlug(item.name) !== product);
+  const shuffledProducts = otherProducts.sort(() => 0.5 - Math.random());
+  const suggestedProducts = shuffledProducts.slice(0, 4);
+  return (
+    <>
+      <ProductPage prod={prod} cat={cat} />
+      <section className="border-r border-base-900">
+        <h2 className="px-4 py-10 md:p-20 border-l border-b border-base-900 text-[26px] font-medium text-center md:text-[38px]">
+          You may also like…
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {suggestedProducts.map((prod, index) => {
+            const catObj = cats.find((item) => item.products.find((q) => CreateSlug(q.name) === CreateSlug(prod.name)));
+            const catHref = catObj ? catObj.href : '';
+            return (
+              <ProductLink key={index} {...prod} src={prod.src[0]} href={`/shop${catHref}/${CreateSlug(prod.name)}`} />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/app/shop/[category]/page.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { ProductLink } from '@/components';
+
+function CreateSlug(string: string) {
+  return string.toLowerCase().replace(/[^a-z0-9]+/g, '-');
+}
+
+interface Product {
+  name: string;
+  price: string;
+  src: string[];
+}
+
+interface Category {
+  category: string;
+  href: string;
+  src2: string;
+  products: Product[];
+}
+
+export async function generateStaticParams() {
+  const cats: Category[] = await fetch('https://norrcollective.vercel.app/api/products').then((res) => res.json());
+
+  return cats.map((cat) => ({
+    category: cat.href.replace('/', ''),
+  }));
+}
+
+export default async function Page({ params }: { params: Promise<{ category: string }> }) {
+  const { category } = await params;
+  const cats: Category[] = await fetch('https://norrcollective.vercel.app/api/products').then((res) => res.json());
+  const cat = cats.find((item) => item.href.replace('/', '') === category.toLowerCase());
+
+  if (!cat) {
+    notFound();
+  }
+
+  return (
+    <>
+      <header className="mt-12.5 md:mt-14.5 lg:mt-23 border-r border-base-900 lg:grid lg:grid-cols-2">
+        <div className="border-l border-b border-base-900 flex items-center justify-center overflow-clip relative lg:sticky lg:top-23 lg:h-[calc(100dvh-92px)] self-start">
+          <Image
+            src={`/images/${cat.src2}`}
+            alt="chair"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-full aspect-[375/420] object-cover md:aspect-[768/500] lg:aspect-auto"
+          />
+          <div className="bg-base-900/50 w-full h-full absolute"></div>
+          <h1 className="text-base-0 text-[40px] font-semibold md:text-[67px] absolute capitalize">{cat.category}</h1>
+        </div>
+        <div className="grid grid-cols-2">
+          {cat.products.map((product, index) => (
+            <ProductLink
+              key={index}
+              {...product}
+              src={product.src[0]}
+              href={`/shop${cat.href}/${CreateSlug(product.name)}`}
+            />
+          ))}
+        </div>
+      </header>
+    </>
+  );
+}
