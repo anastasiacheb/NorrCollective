1dd8fe33659dc518e1b4e08477befb8fa33cdd8f
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-08-10 22:06:45 +0300

    toast

--- src/components/ProductLink.tsx
+++ src/components/ProductLink.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { Animation } from '@/components';
 
 interface ProductLinkProps {
   name: string;
@@ -12,15 +13,17 @@ export default function ProductLink({ name, price, src, href }: ProductLinkProps
     <Link
       href={href}
       className="lg:h-[calc((100dvh-92px)/2)] border-l border-b border-base-900 group overflow-clip relative">
-      <Image
-        loading="eager"
-        src={`/images/${src}`}
-        alt="chair"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="w-full h-full aspect-square object-cover lg:aspect-auto group-hover:scale-110 transition-all ease-linear duration-200"
-      />
+      <Animation scale className=" w-full h-full">
+        <Image
+          loading="eager"
+          src={`/images/${src}`}
+          alt="chair"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-full aspect-square object-cover lg:aspect-auto group-hover:scale-110 transition-all ease-linear duration-200"
+        />
+      </Animation>
       <div className="absolute z-40 bottom-3 text-center flex flex-col gap-1 justify-center items-center md:bottom-6 w-full">
         <h2 className="text-base font-medium leading-none backdrop-blur-sm rounded-full">{name}</h2>
         <p className="text-base-500 text-sm font-medium backdrop-blur-sm rounded-full">price {price}$</p>
--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { Slider, Counter, RadioButton } from '@/components';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '@/components/CartContext';
 
 interface Product {
@@ -25,50 +25,75 @@ interface Props {
 
 export default function ProductPage({ prod, cat }: Props) {
   const [quantity, setQuantity] = useState(1);
+  const [toastIsVisible, setToastIsVisible] = useState(false);
+  const [firstAdd, setFirstAdd] = useState(true);
 
   const cart = useContext(CartContext);
   if (!cart) return null;
   const { cartProducts, setCartProducts } = cart;
 
+  useEffect(() => {
+    if (!toastIsVisible) return;
+
+    const timer = setTimeout(() => {
+      setToastIsVisible(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [toastIsVisible, setToastIsVisible]);
+
   function handleAdd() {
+    setToastIsVisible(true);
     if (cartProducts.find((item) => item.name === prod.name)) {
-      alert('Already added to cart');
+      setFirstAdd(false);
     } else {
       setCartProducts([...cartProducts, { src: prod.src[0], name: prod.name, quantity: quantity, price: prod.price }]);
+      setFirstAdd(true);
     }
   }
 
   return (
-    <section className="mt-12.5 md:mt-14.5 lg:mt-23 border-r border-base-900 lg:grid lg:grid-cols-2 lg:h-[calc(100dvh-92px)]">
-      <Slider Images={prod.src} />
-      <div className="border-l border-b border-base-900 px-4 py-10 md:p-10 flex flex-col gap-6 justify-between">
-        <div className="flex flex-col gap-6">
-          <div className="uppercase text-xs md:text-sm font-medium">
-            <Link href={`/shop${cat.href}`}>{cat.category}</Link> / <span className="text-base-500">{prod.name}</span>
-          </div>
-          <div>
-            <h1 className="capitalize font-medium text-[26px] md:text-[38px] mb-4">
-              {prod.name.toLowerCase()} - ${prod.price}
-            </h1>
-            <p className="leading-snug">{prod.description}</p>
-          </div>
-          <div className="flex flex-col gap-6 md:flex-row md:gap-4 md:items-center">
-            <h2 className="font-medium text-lg leading-snug">Quantity</h2>
-            <Counter quantity={quantity} setQuantity={setQuantity} />
-          </div>
-        </div>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-4">
-            <h2 className="font-medium text-lg leading-snug">Purchase type</h2>
-            <RadioButton />
-          </div>
-          <button
-            onClick={handleAdd}
-            className="bg-base-900 text-base-0 hover:bg-base-700 transition-all ease-linear uppercase text-sm leading-none font-medium border border-base-900 md:text-base h-12 md:h-14 flex items-center justify-center">
-            Add to basket
-          </button>
-        </div>
+    <>
+      <div
+        className={`text-base-0 bg-base-900 flex p-4 gap-1 items-center w-fit absolute top-16 md:top-17 left-1/2 -translate-x-1/2 z-50 lg:right-10 lg:translate-0 lg:bottom-30 lg:top-auto lg:left-auto ${toastIsVisible ? 'opacity-100' : 'opacity-0'} transition-opacity duration-200 ease-linear`}>
+        <img
+          src={`/icons/${firstAdd ? 'Correct.svg' : 'error-warning.svg'}`}
+          alt={`${firstAdd ? 'success' : 'error'} icon`}
+          className="size-4"
+        />
+        <p>{firstAdd ? 'Added to cart' : 'Already added to cart'}</p>
       </div>
-    </section>
+      <section className="mt-12.5 md:mt-14.5 lg:mt-23 border-r border-base-900 lg:grid lg:grid-cols-2 lg:h-[calc(100dvh-92px)]">
+        <Slider Images={prod.src} />
+        <div className="border-l border-b border-base-900 px-4 py-10 md:p-10 flex flex-col gap-6 justify-between">
+          <div className="flex flex-col gap-6">
+            <div className="uppercase text-xs md:text-sm font-medium">
+              <Link href={`/shop${cat.href}`}>{cat.category}</Link> / <span className="text-base-500">{prod.name}</span>
+            </div>
+            <div>
+              <h1 className="capitalize font-medium text-[26px] md:text-[38px] mb-4">
+                {prod.name.toLowerCase()} - ${prod.price}
+              </h1>
+              <p className="leading-snug">{prod.description}</p>
+            </div>
+            <div className="flex flex-col gap-6 md:flex-row md:gap-4 md:items-center">
+              <h2 className="font-medium text-lg leading-snug">Quantity</h2>
+              <Counter quantity={quantity} setQuantity={setQuantity} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-4">
+              <h2 className="font-medium text-lg leading-snug">Purchase type</h2>
+              <RadioButton />
+            </div>
+            <button
+              onClick={handleAdd}
+              className="bg-base-900 text-base-0 hover:bg-base-700 transition-all ease-linear uppercase text-sm leading-none font-medium border border-base-900 md:text-base h-12 md:h-14 flex items-center justify-center">
+              Add to basket
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
