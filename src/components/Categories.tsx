61cac115159ea9fc4b9687a5328549ab11405a81
Anastasia <161876455+anastasiacheb@users.noreply.github.com>
2025-08-11 04:14:51 +0300

    main speed fix 2

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -30,7 +30,7 @@ function CategoryItem({ category, src1, href, index }: CategoryItemProps) {
         <Animation scale className=" w-full h-full">
           <Image
             loading="eager"
-            priority
+            priority={index === 0 || index === 1}
             src={`/images/${src1}`}
             alt="chair"
             width={0}
